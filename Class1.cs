7df469b1aebd7464212ad24dd5cce5ddd08b1147
NARESH <64158150+NARESH-JPG@users.noreply.github.com>
2021-10-14 13:40:05 +0530

    Day_1

---
+++ Class1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_Class_Implementation
+{
+    partial  class LMS : IAsessment
+    {
+        string name;
+        int SNo;
+        long MobileNo;
+        string Email;
+         public void Display()
+        {
+            Console.WriteLine("Implementing Void");
+        }
+        void IAsessment.MCQ_Assesment()
+        {
+            Console.WriteLine("This implemtation is from LMS Class using assesment INterace");
+        } // Defining Methods declared interface 
+        void IAsessment.QuestionBank()
+        {
+            Console.WriteLine(" Returning value from Question bank defined inside LMS Class");
+        }    // Defining Methods declared interface 
+    }
+
+     partial class LMS :  Placement, IAsessment // Inheritance
+    {
+        public void Message()
+        {
+            Console.WriteLine("This message is coming from Seocond implemtation of Class" );
+            }
+
+    }
+
+    abstract class Placement// Can't create object of the class
+    {
+        string Name_Of_Company;
+        int No_of_Student_Placed;
+        DateTime Year_of_placement;
+
+        public void Placement_record()
+        {
+            Console.WriteLine("This method will show you complete record of the placement");        
+        }
+    }
+
+    public interface IAsessment
+        { 
+
+            //int Assessment_Count; creating instance field is not allowed
+            //Only delaration is allowed inside Interface 
+            public void MCQ_Assesment();
+             public void QuestionBank();
+        
+
+        }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Demo_Class_Implementation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LMS Akash = new LMS();
+            Akash.Display();
+            Akash.Message();
+            Akash.Placement_record();
+
+
+
+            IAsessment Delhi = Akash; // we created refrence of Interface so that it can store refrence of the LMS class object 
+
+            Delhi.MCQ_Assesment();
+            Delhi.QuestionBank();
+        
+    
+
+           // Placement NIT = new Placement();
+
+        }
+    }
+}
